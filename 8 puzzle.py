e4c2961235bf9b41d588a2fb72603bfcabaff399
ranabhumi2022 <ranabhumi2022@gmail.com>
2026-06-07 16:04:19 +0530

    Update 8 puzzle.py

--- 8 puzzle.py
+++ 8 puzzle.py
@@ -1,30 +1,18 @@
 from collections import deque 
 
-
-# ---------------- GOAL STATE ----------------//This is the final desired arrangement of the puzzle.
-
-
 GOAL_STATE = (
     (1, 2, 3),
     (4, 5, 6),
     (7, 8, 0)
 )
 
-# Possible movements of the blank tile  //Defines how the blank tile can move: Up/Down/LefT/Right
 MOVES = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-
-# ---------------- FIND BLANK TILE ----------------//Finds the row and column position of the blank tile (0). Needed to know where moves are possible.
 
 def find_blank(state):
     for i in range(3):
         for j in range(3):
             if state[i][j] == 0:
                 return i, j
-
-
-# ---------------- GENERATE NEXT STATES ----------------// Creates all possible next puzzle states.
-
 def generate_neighbors(state):
     neighbors = []
     x, y = find_blank(state)
@@ -38,10 +26,6 @@ def generate_neighbors(state):
             neighbors.append(tuple(tuple(row) for row in new_state))
 
     return neighbors
-
-
-# ---------------- BFS ALGORITHM ----------------// This is the Breadth First Search algorithm.
-
 def bfs(start_state):
     queue = deque([start_state])
     visited = {start_state}
@@ -59,9 +43,6 @@ def bfs(start_state):
                 queue.append(neighbor)
 
     return None
-
-
-# ---------------- RECONSTRUCT PATH ----------------// Builds the solution path from goal to start using parent links.
 def reconstruct_path(parent, state):
     path = []
     while state is not None:
@@ -69,17 +50,13 @@ def reconstruct_path(parent, state):
         state = parent[state]
     path.reverse()
     return path
-
-
-# ---------------- PRINT SOLUTION ----------------//Prints each puzzle configuration step-by-step.
-def print_solution(path):
     for step in path:
         for row in step:
             print(row)
         print()
 
 
-# ---------------- DRIVER CODE ----------------
+# ---------------- DRIVER CODE ---------------
 if __name__ == "__main__":
     start_state = (
         (1, 2, 3),
